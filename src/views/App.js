877a2617463c6a769f00e40324a0e5eff66d6eb3
GensCodeB0t <gensy83@gmail.com>
2020-02-03 15:52:43 -0600

    initial commit

---
+++ src/views/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppMap from '../components/map';
+import WithMapMarkerService from '../services/map_marker';
+import WithApiService from '../services/api';
+
+export default function App(){
+    return(
+      <div className="App">
+        HELLO WORLD
+      </div>
+    );
+  }
+
